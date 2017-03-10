3b57fecced59d6e6203e95a60a1c59b1f9c33cf3
steppedreckoner <gdcousins@gmail.com>
2017-03-09 20:02:11 -0600

    Finished iterative zMaxes for Path class

--- src/main/java/com/sidepiece/model/Path.java
+++ src/main/java/com/sidepiece/model/Path.java
@@ -4,8 +4,6 @@ import java.util.Comparator;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import org.springframework.util.CollectionUtils;
-
 public class Path {
 	
 	/*
@@ -16,18 +14,44 @@ public class Path {
 	private Double zPowMax;
 	// TODO: Usefulness of two maximums for two-phase lifts (n-phase lifts?)
 	
+	// Max value is 32767 -- plenty
+	private Short mass;
+	
 	SortedMap<Double, Point> timeToPosition = new TreeMap<>();
 	SortedMap<Double, Point> timeToVelocity = new TreeMap<>();
 	SortedMap<Double, Point> timeToAcceleration = new TreeMap<>();
 	
-	public double getMaxVelocity_iterative(Comparator<Point> comparator) {
-		if (zVelMax != null)
-			return zVelMax;
-		
-		return 0;
+	public Double getZVelMax() {
+		if (zVelMax == null)
+			zVelMax = getMaxVelocityPoint_iterative(Point.zComp).getZVal();
+		return zVelMax;
 	}
 	
-	private void generateTimeToVelocity() {
+	public Double getZMaxAcc() {
+		if (zAccMax == null)
+			zAccMax = getMaxAccelerationPoint_iterative(Point.zComp).getZVal();
+		return zAccMax;
+	}
+	
+	public Double getZPowMax() {
+		if (zPowMax == null)
+			zPowMax = getMaxAccelerationPoint_iterative(Point.zComp).getZVal() * mass;
+		return zPowMax;
+	}
+	
+	private Point getMaxVelocityPoint_iterative(Comparator<Point> comparator) {
+		if (timeToVelocity == null)
+			generateTimeToVelocity_iterative();
+		
+		Point max = null;
+		for (Point p : timeToVelocity.values()) {
+			if (max == null || comparator.compare(p, max) == 1)
+				max = p;
+		}
+		return max;
+	}
+	
+	private void generateTimeToVelocity_iterative() {
 		
 		Double prevTime = null;
 		Point prevPos = null;
@@ -43,6 +67,38 @@ public class Path {
 			}
 			prevTime = time;
 			prevPos = currentPos;
+		}
+	}
+	
+	private Point getMaxAccelerationPoint_iterative(Comparator<Point> comparator) {
+		if (timeToVelocity == null)
+			generateTimeToVelocity_iterative();
+		if (timeToAcceleration == null)
+			generateTimeToAcceleration_iterative();
+		
+		Point max = null;
+		for (Point p : timeToVelocity.values()) {
+			if (max == null || comparator.compare(p, max) == 1)
+				max = p;
+		}
+		return max;
+	}
+	
+	private void generateTimeToAcceleration_iterative() {
+		Double prevTime = null;
+		Point prevVel = null;
+		
+		// Should give keys in sorted order
+		for (Double time : timeToVelocity.keySet()) {
+			Point currentVel = timeToVelocity.get(time);
+			if (prevTime == null)
+				timeToAcceleration.put(time, new Point(0d, 0d, 0d));
+			else {
+				Double dT = time - prevTime;
+				timeToAcceleration.put(time, Point.makeDerivativePoint(prevVel, currentVel, dT));
+			}
+			prevTime = time;
+			prevVel = currentVel;
 		}
 	}
 
