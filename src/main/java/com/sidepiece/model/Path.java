867ff80e8ac5000ff0aaa5bbf07eb4091ff5fbfb
steppedreckoner <gdcousins@gmail.com>
2017-03-09 20:02:25 -0600

    formatting

--- src/main/java/com/sidepiece/model/Path.java
+++ src/main/java/com/sidepiece/model/Path.java
@@ -5,7 +5,7 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class Path {
-	
+
 	/*
 	 * The output data we care about from this object
 	 */
@@ -13,36 +13,36 @@ public class Path {
 	private Double zAccMax;
 	private Double zPowMax;
 	// TODO: Usefulness of two maximums for two-phase lifts (n-phase lifts?)
-	
+
 	// Max value is 32767 -- plenty
 	private Short mass;
-	
+
 	SortedMap<Double, Point> timeToPosition = new TreeMap<>();
 	SortedMap<Double, Point> timeToVelocity = new TreeMap<>();
 	SortedMap<Double, Point> timeToAcceleration = new TreeMap<>();
-	
+
 	public Double getZVelMax() {
 		if (zVelMax == null)
 			zVelMax = getMaxVelocityPoint_iterative(Point.zComp).getZVal();
 		return zVelMax;
 	}
-	
+
 	public Double getZMaxAcc() {
 		if (zAccMax == null)
 			zAccMax = getMaxAccelerationPoint_iterative(Point.zComp).getZVal();
 		return zAccMax;
 	}
-	
+
 	public Double getZPowMax() {
 		if (zPowMax == null)
 			zPowMax = getMaxAccelerationPoint_iterative(Point.zComp).getZVal() * mass;
 		return zPowMax;
 	}
-	
+
 	private Point getMaxVelocityPoint_iterative(Comparator<Point> comparator) {
 		if (timeToVelocity == null)
 			generateTimeToVelocity_iterative();
-		
+
 		Point max = null;
 		for (Point p : timeToVelocity.values()) {
 			if (max == null || comparator.compare(p, max) == 1)
@@ -50,12 +50,12 @@ public class Path {
 		}
 		return max;
 	}
-	
+
 	private void generateTimeToVelocity_iterative() {
-		
+
 		Double prevTime = null;
 		Point prevPos = null;
-		
+
 		// Should give keys in sorted order
 		for (Double time : timeToPosition.keySet()) {
 			Point currentPos = timeToPosition.get(time);
@@ -69,13 +69,13 @@ public class Path {
 			prevPos = currentPos;
 		}
 	}
-	
+
 	private Point getMaxAccelerationPoint_iterative(Comparator<Point> comparator) {
 		if (timeToVelocity == null)
 			generateTimeToVelocity_iterative();
 		if (timeToAcceleration == null)
 			generateTimeToAcceleration_iterative();
-		
+
 		Point max = null;
 		for (Point p : timeToVelocity.values()) {
 			if (max == null || comparator.compare(p, max) == 1)
@@ -83,11 +83,11 @@ public class Path {
 		}
 		return max;
 	}
-	
+
 	private void generateTimeToAcceleration_iterative() {
 		Double prevTime = null;
 		Point prevVel = null;
-		
+
 		// Should give keys in sorted order
 		for (Double time : timeToVelocity.keySet()) {
 			Point currentVel = timeToVelocity.get(time);
