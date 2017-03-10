8c01a47fc202254a3ae77e1f89517e2b26393f29
steppedreckoner <gdcousins@gmail.com>
2017-03-09 18:46:44 -0600

    Initialized Project

---
+++ src/main/java/com/sidepiece/BarSpeedApplication.java
@@ -0,0 +1,12 @@
+package com.sidepiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarSpeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarSpeedApplication.class, args);
+	}
+}
